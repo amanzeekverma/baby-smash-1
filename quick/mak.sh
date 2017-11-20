6e06eb313a552f8f69f48d1371e6b93d8fac486a
Tushar Kapila <tgkprog@gmail.com>
2017-11-20 21:26:04 +0530

    new dir layout and build file under quick/mak.sh

---
+++ quick/mak.sh
@@ -0,0 +1,20 @@
+#this script works on Ubuntu from the baby-smash/quick directory
+
+
+
+mkdir bin
+#incase its alteady there
+rm bin/*.class
+echo Dir contents after clean
+ls
+sleep 2
+echo
+echo now compile
+javac -d bin -cp src src/*.java
+cd bin
+jar -cm ../src/mani.m    *.class >../BabySmashApp.jar
+echo Done
+cd ..
+echo Dir contents
+ls
+
---
+++ quick/src/mani.m
@@ -0,0 +1,4 @@
+Main-Class: BabySmash
+X-Author: Tushar Kapila 2012 Copyright
+X-website: http://sel2in.com
+
